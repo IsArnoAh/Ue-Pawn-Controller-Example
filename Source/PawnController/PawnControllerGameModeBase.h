a7ee59c8d725712b9778ecd0c516a32db93b6fcc
IsArnoAh <2286430915@qq.com>
2023-04-18 08:08:39 +0800

    项目初始化

---
+++ Source/PawnController/PawnControllerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PawnControllerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAWNCONTROLLER_API APawnControllerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
